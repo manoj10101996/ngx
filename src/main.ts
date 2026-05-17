daedbd4c3eb62d699016c4b874d086a881f2032d
Manojkumar Muthukumar <19234862+manoj10101996@users.noreply.github.com>
2026-05-17 08:14:15 +0530

    Updates

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 
 
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+  providers: [provideZoneChangeDetection(),importProvidersFrom(BrowserModule, AppRoutingModule)]
 })
   .catch(err => console.error(err));
